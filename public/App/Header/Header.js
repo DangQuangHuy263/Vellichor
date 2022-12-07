2a863390caa63408224f1734f978d1ecb8af8b5b
PA <ngophuonganh9a4.ptin@gmail.com>
2022-12-07 16:28:01 +0700

    update

--- public/App/Header/Header.js
+++ public/App/Header/Header.js
@@ -1,3 +1,6 @@
+function mySearch() {
+    window.location.href = "../Shop/Shop.html";
+}
 function myAccount() {
     // window.open("../Account/Account.html");
     window.location.href = "../Account/Account.html";
